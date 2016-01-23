f776653ac1c968f99656553e6efc799104b934f8
Chathu Abeywickrama <chathu.abey93@gmail.com>
2016-01-23 19:12:35 +0530

    Added Util Folder Containing DB connect files
    These files contain the DB connect file
    added some comments to say what the codes in the DB connect does like
    select query and insert update, delete query

---
+++ ITP_RMSS/Util/Connection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ITP_RMSS.Util
+{
+    class Connection
+    {
+        private static MySqlConnection conn;
+        private static String server = "localhost;";
+        private static String database = "itp_rmss;";
+        private static String username = "root;";
+        private static String password = "1234;";
+
+        public static MySqlConnection getConnection()
+        {
+            String connectionString = "Server=" + server + "Database=" + database + "Username=" + username + "Password=" + password;
+
+            conn = new MySqlConnection(connectionString);
+            conn.Open();
+
+            if (conn.State.ToString() == "Open")
+            {
+                return conn;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ ITP_RMSS/Util/DBHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITP_RMSS.Util;
+using MySql.Data.MySqlClient;   
+
+namespace ITP_RMSS.Util
+{
+    class DBHandler
+    {
+        static MySqlConnection conn = Connection.getConnection();
+
+        //This piece of code is for insert, Update and delete query
+
+        public static bool ExecuteNonQuery(String query)
+        {
+            if (conn != null)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        //This piece of code is for the select query
+        public static MySqlDataReader ExecuteQuery(String query)
+        {
+            if (conn != null)
+            {
+                try
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    return cmd.ExecuteReader();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return null;
+                }
+            }
+            return null;
+        }
+
+    }
+}
